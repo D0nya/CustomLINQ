a0c3d80b75795ceb39fc78bd380dff4f86f3cf5e
D0nya <dboeshko@gmail.com>
2019-08-07 21:54:31 +0300

    Changed Iterator realization

--- Iterator/CustomList.cs
+++ Iterator/CustomList.cs
@@ -1,27 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace Iterator
 {
-  public class CustomList<T> : IIteratable<T>
+  public class CustomList<T> : IIteratable<T>, IEnumerator<T>
   {
     private List<T> list;
+    private T current;
+    private int state = 0;
+    private int i;
+
     public CustomList()
     {
       list = new List<T>();
     }
     public int Count { get { return list.Count; } }
-    public void Add(T item)
-    {
-      list.Add(item);
-    }
-    public List<T> GetItems()
-    {
-      return list;
-    }
-    public void Clear()
-    {
-      list.Clear();
-    }
+    public T Current { get { return current; } }
+    object IEnumerator.Current { get { return current; } }
+
     public T this[int index]
     {
       get { return list[index]; }
@@ -34,9 +30,55 @@ namespace Iterator
         list.Add(item);
       }
     }
+    public List<T> GetItems()
+    {
+      return list;
+    }
+    public void Add(T item)
+    {
+      list.Add(item);
+    }
+    public void Clear()
+    {
+      list.Clear();
+    }
     public IEnumerator<T> GetEnumerator()
     {
-      return list.GetEnumerator();
+      return this;
+    }
+
+    public bool MoveNext()
+    {
+      switch(state)
+      {
+        case 0:
+          i = 0;
+          state = 1;
+          goto case 1;
+        case 1:
+          if (!(i < Count))
+          {
+            Reset();
+            return false;
+          }
+          current = list[i];
+          state = 2;
+          return true;
+        case 2:
+          ++i;
+          goto case 1;
+      }
+      return false;
+    }
+
+    public void Reset()
+    {
+      state = 0;
+      i = 0;
+      current = list[i];
+    }
+    public void Dispose()
+    {
     }
   }
 }
--- Iterator/IteratorExtensions.cs
+++ Iterator/IteratorExtensions.cs
@@ -2,74 +2,40 @@
 
 namespace Iterator
 {
-  static class IteratorExtensions 
+  public static class IteratorExtensions 
   {
-    /// <summary>
-    /// Returns the first value match the predicate or default if nothing found
-    /// </summary>
-    /// <typeparam name="TSource"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
-    public static TSource FirstOrDefault<TSource>(this IIteratable<TSource> source, Func<TSource, bool> predicate)
-    {
-      if (source == null)
-        return default;
-      foreach (TSource item in source)
-      {
-        if (predicate(item))
-          return item;
-      }
-      return default;
-    }
-    /// <summary>
-    /// Returns IIteratable with elements, match the predicate
-    /// </summary>
-    /// <typeparam name="TSource"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
     public static IIteratable<TSource> Where<TSource>(this IIteratable<TSource> source, Func<TSource, bool> predicate)
     {
       if (source == null)
-        return null;
-      CustomList<TSource> result = new CustomList<TSource>();
-      foreach (TSource item in source)
-      {
-        if (predicate(item))
-          result.Add(item);
-      }
-      return result;
+        throw new ArgumentNullException("source");
+      var ret = new WhereEnumerable<TSource>(source, predicate);
+      return ret;
     }
-    /// <summary>
-    /// Returns IIteratable with elements, processed by the predicate
-    /// </summary>
-    /// <typeparam name="TSource"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
-    public static IIteratable<TSource> Map<TSource>(this IIteratable<TSource> source, Func<TSource, TSource> predicate)
+
+    public static IIteratable<TSource> Map<TSource>(this IIteratable<TSource> source, Func<TSource, TSource> func)
     {
       if (source == null)
-        return null;
-      CustomList<TSource> result = new CustomList<TSource>();
+        throw new ArgumentNullException("source");
+      var ret = new MapEnumerable<TSource>(source, func);
+      return ret;
+    }
+
+    public static int Count<TSource>(this IIteratable<TSource> source)
+    {
+      if (source == null)
+        throw new ArgumentNullException("source");
+      int cnt = 0;
       foreach (TSource item in source)
       {
-        result.Add(predicate(item));
+        cnt++;
       }
-      return result;
+      return cnt;
     }
-    /// <summary>
-    /// Returns true, if there any elements match the predicate
-    /// </summary>
-    /// <typeparam name="TSource"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
+
     public static bool Some<TSource>(this IIteratable<TSource> source, Func<TSource, bool> predicate)
     {
       if (source == null)
-        return false;
+        throw new ArgumentNullException("source");
       foreach (TSource item in source)
       {
         if (predicate(item))
@@ -77,17 +43,12 @@ namespace Iterator
       }
       return false;
     }
-    /// <summary>
-    /// Returns true, if all elements match the predicate
-    /// </summary>
-    /// <typeparam name="TSource"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
+
     public static bool All<TSource>(this IIteratable<TSource> source, Func<TSource, bool> predicate)
     {
       if (source == null)
-        return true;
+        throw new ArgumentNullException("source");
+
       foreach (TSource item in source)
       {
         if (!predicate(item))
@@ -95,106 +56,50 @@ namespace Iterator
       }
       return true;
     }
-    /// <summary>
-    /// Returns number of elements in IITeratable
-    /// </summary>
-    /// <typeparam name="TSource"></typeparam>
-    /// <param name="source"></param>
-    /// <returns></returns>
-    public static int Count<TSource>(this IIteratable<TSource> source)
-    {
-      if (source == null)
-        return 0;
-      int count = 0;
-      foreach (var item in source)
-      {
-        count++;
-      }
-      return count;
-    }
-    /// <summary>
-    /// Concatenates two sequences
-    /// </summary>
-    /// <typeparam name="TSource"></typeparam>
-    /// <param name="first"></param>
-    /// <param name="second"></param>
-    /// <returns></returns>
-    public static IIteratable<TSource> Concat<TSource>(this IIteratable<TSource> first, IIteratable<TSource> second)
-    {
-      if (first == null || second == null)
-        throw new ArgumentNullException();
-      CustomList<TSource> result = new CustomList<TSource>();
-      foreach (TSource item in first)
-      {
-        result.Add(item);
-      }
-      foreach (TSource item in second)
-      {
-        result.Add(item);
-      }
-      return result;
-    }
-    /// <summary>
-    /// Returns IIteratable with items selected by selector
-    /// </summary>
-    /// <typeparam name="TSource"></typeparam>
-    /// <typeparam name="TResult"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="selector"></param>
-    /// <returns></returns>
-    public static IIteratable<TResult> Select<TSource, TResult>(this IIteratable<TSource> source, Func<TSource, TResult> selector)
-    {
-      if (source == null)
-        return null;
-      CustomList<TResult> result = new CustomList<TResult>();
-      foreach (var item in source)
-      {
-        result.Add(selector(item));
-      }
-      return result;
-    }
-    /// <summary>
-    /// Converts IITeratable to <typeparamref name="TSource"/>[] array.
-    /// </summary>
-    /// <typeparam name="TSource"></typeparam>
-    /// <param name="source"></param>
-    /// <returns></returns>
+
     public static TSource[] ToArray<TSource>(this IIteratable<TSource> source)
     {
-      if (source == null)
-        return null;
-      TSource[] array = new TSource[source.Count()];
+      TSource[] ret = new TSource[source.Count()];
       int i = 0;
       foreach (TSource item in source)
       {
-        array[i] = item;
+        ret[i] = item;
         i++;
       }
-      return array;
+      return ret;
     }
-    /// <summary>
-    /// Returns IIteratable ordered by selected key
-    /// </summary>
-    /// <typeparam name="TSource"></typeparam>
-    /// <typeparam name="TKey"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="keySelector"></param>
-    /// <returns></returns>
-    public static IIteratable<TSource> OrderBy<TSource, TKey>(this IIteratable<TSource> source, Func<TSource, TKey> keySelector)
+
+    public static TSource FirstOrDefault<TSource>(this IIteratable<TSource> source, Func<TSource, bool> predicate)
     {
       if (source == null)
-        return null;
-
-      var items = source.ToArray();
-      var keys = source.Select(keySelector).ToArray();
-      Array.Sort(keys, items);
-
-      CustomList<TSource> result = new CustomList<TSource>();
-      foreach (var item in items)
+        throw new ArgumentNullException("source");
+      foreach (TSource item in source)
       {
-        result.Add(item);
+        if (predicate(item))
+          return item;
       }
-      return result;
+      return default;
+    }
+
+    public static IIteratable<TSource> Concat<TSource>(this IIteratable<TSource> first, IIteratable<TSource> second)
+    {
+      if (first == null)
+        throw new ArgumentNullException("first");
+      if (second == null)
+        throw new ArgumentNullException("second");
+
+      var ret = new ConcatEnumerable<TSource>(first, second);
+      return ret;
+    }
+
+    public static IIteratable<TReturn> Select<TSource, TReturn>(this IIteratable<TSource> source, Func<TSource, TReturn> selector)
+    {
+      if (source == null)
+        throw new ArgumentNullException("source");
+
+      var ret = new SelectEnumerable<TSource, TReturn>(source, selector);
+
+      return ret;
     }
   }
 }
